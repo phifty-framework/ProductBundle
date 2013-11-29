eb54b31cd575fb7d8fe5110de569206c9be838ce
c9s <cornelius.howl@gmail.com>
2013-11-29 18:35:45 +0800

    Clean up generated files.

--- Model/ProductRecipeCollectionBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ProductBundle\Model;
-
-class ProductRecipeCollectionBase  extends \LazyRecord\BaseCollection {
-const schema_proxy_class = '\\ProductBundle\\Model\\ProductRecipeSchemaProxy';
-const model_class = '\\ProductBundle\\Model\\ProductRecipe';
-const table = 'product_recipes';
-
-}
