6ed7dea9a545fe7a15aa18513173bde1cb432a4b
c9s <cornelius.howl@gmail.com>
2014-04-23 11:51:31 +0800

    BulkCopyProduct: add subsections copy

--- Action/BulkCopyProduct.php
+++ Action/BulkCopyProduct.php
@@ -20,32 +20,51 @@ class BulkCopyProduct extends BulkCopyRecordAction
             ));
         }
 
-        $images = $record->images;
-        $images->fetch();
-
-        foreach( $images as $image ) {
-            $imageArgs = $image->getData();
-            unset($imageArgs['id']);
-            $imageArgs['product_id'] = $newRecord->id;
-            $newImage = new \ProductBundle\Model\ProductImage;
-            $ret = $newImage->create($imageArgs);
-            if ( ! $ret->success ) {
-                return $this->error('產品圖片複製錯誤');
+        if ( isset($record->images) ) {
+            $images = $record->images;
+            $images->fetch();
+            foreach( $images as $image ) {
+                $imageArgs = $image->getData();
+                unset($imageArgs['id']);
+                $imageArgs['product_id'] = $newRecord->id;
+                $newImage = new \ProductBundle\Model\ProductImage;
+                $ret = $newImage->create($imageArgs);
+                if ( ! $ret->success ) {
+                    return $this->error('產品圖片複製錯誤');
+                }
             }
         }
 
-        $resources = $record->resources;
-        $resources->fetch();
-        foreach( $resources as $resource ) {
-            $resourceArgs = $resource->getData();
-            unset($resourceArgs['id']);
-            $resourceArgs['product_id'] = $newRecord->id;
-            $newResource = new \ProductBundle\Model\Resource;
-            $ret = $newResource->create($resourceArgs);
-            if ( ! $ret->success ) {
-                return $this->error('產品圖片複製錯誤');
+        if ( isset($record->resources) ) {
+            $resources = $record->resources;
+            $resources->fetch();
+            foreach( $resources as $resource ) {
+                $resourceArgs = $resource->getData();
+                unset($resourceArgs['id']);
+                $resourceArgs['product_id'] = $newRecord->id;
+                $newResource = new \ProductBundle\Model\Resource;
+                $ret = $newResource->create($resourceArgs);
+                if ( ! $ret->success ) {
+                    return $this->error('產品資源複製錯誤');
+                }
             }
         }
+
+        if ( isset($record->subsections) ) {
+            $subsections = $record->subsections;
+            $subsections->fetch();
+            foreach( $subsections as $subsection ) {
+                $subsectionArgs = $subsection->getData();
+                unset($subsectionArgs['id']);
+                $subsectionArgs['product_id'] = $newRecord->id;
+                $newRecord = new \ProductBundle\Model\ProductSubsection;
+                $ret = $newRecord->create($subsectionArgs);
+                if ( ! $ret->success ) {
+                    return $this->error('產品資源複製錯誤');
+                }
+            }
+        }
+
     }
 }
 
