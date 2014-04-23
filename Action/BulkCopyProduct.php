81c4030dfbd1061b37abf466aa4eee11a5bc048f
c9s <cornelius.howl@gmail.com>
2014-04-23 15:59:18 +0800

    Add function for bulk copy record with files

--- Action/BulkCopyProduct.php
+++ Action/BulkCopyProduct.php
@@ -1,6 +1,23 @@
 <?php
 namespace ProductBundle\Action;
 use ActionKit\RecordAction\BulkCopyRecordAction;
+use Phifty\FileUtils;
+
+function duplicate_file($from) {
+    $to = FileUtils::filename_increase($from);
+    copy($from , $to);
+    return $to;
+}
+
+function duplicate_record_files($source, $fields) {
+    $output = $source;
+    foreach( $fields as $field ) {
+        if ( isset($output[$field]) ) {
+            $output[$field] = duplicate_file($output[$field]);
+        }
+    }
+    return $output;
+}
 
 class BulkCopyProduct extends BulkCopyRecordAction
 {
@@ -24,10 +41,11 @@ class BulkCopyProduct extends BulkCopyRecordAction
             $images = $record->images;
             $images->fetch();
             foreach( $images as $image ) {
-                $imageArgs = $image->getData();
+                $imageArgs = duplicate_record_files($image->getData(), [ 'image','thumb' ]);
                 unset($imageArgs['id']);
                 $imageArgs['product_id'] = $newRecord->id;
                 $newImage = new \ProductBundle\Model\ProductImage;
+
                 $ret = $newImage->create($imageArgs);
                 if ( ! $ret->success ) {
                     return $this->error('產品圖片複製錯誤');
