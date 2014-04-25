6971ec2f52536abf3e6d3970546b2354cd295242
c9s <yoanlin93@gmail.com>
2014-04-25 19:55:55 +0800

    Clean up BulkCopyProduct action

--- Action/BulkCopyProduct.php
+++ Action/BulkCopyProduct.php
@@ -1,23 +1,6 @@
 <?php
 namespace ProductBundle\Action;
 use ActionKit\RecordAction\BulkCopyRecordAction;
-use Phifty\FileUtils;
-
-function duplicate_file($from) {
-    $to = FileUtils::filename_increase($from);
-    copy($from , $to);
-    return $to;
-}
-
-function duplicate_record_files($source, $fields) {
-    $output = $source;
-    foreach( $fields as $field ) {
-        if ( isset($output[$field]) ) {
-            $output[$field] = duplicate_file($output[$field]);
-        }
-    }
-    return $output;
-}
 
 class BulkCopyProduct extends BulkCopyRecordAction
 {
