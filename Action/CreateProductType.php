1f24bf92fbab048945b68ab353e1c8d65f5992bc
c9s <yoanlin93@gmail.com>
2014-10-17 16:40:26 +0800

    Add CreateProductType action

---
+++ Action/CreateProductType.php
@@ -0,0 +1,43 @@
+<?php
+namespace ProductBundle\Action;
+use ActionKit\RecordAction\CreateRecordAction;
+use ProductBundle\Model\ProductType;
+
+class CreateProductType extends CreateRecordAction
+{
+    public $recordClass = 'ProductBundle\\Model\\ProductType';
+
+    public function schema()
+    {
+        $this->useRecordSchema();
+        $bundle = kernel()->bundle('ProductBundle');
+        $autoResize = $bundle->config('auto_resize');
+        $iconSize = $bundle->config('ProductType.icon.size');
+        $iconSizeLimit = $bundle->config('ProductType.icon.size_limit');
+        $iconResizeWidth = $bundle->config('ProductType.icon.resize_width') ?: 0;
+        if ($bundle->config('ProductType.icon')) {
+            $this->param('icon','Image')
+                ->size($iconSize)
+                ->autoResize($autoResize)
+                ->resizeWidth($iconResizeWidth)
+                ->label('圖示')
+                ->hint($bundle->config('ProductType.icon.hint'))
+                ->hintFromSizeInfo()
+                ;
+        }
+    }
+
+    public function run() {
+        // kernel()->db->query("LOCK TABLES " . ProductType::table . " AS m WRITE");
+        $ret = parent::run();
+        // kernel()->db->query("UNLOCK TABLES");
+        return $ret;
+    }
+
+    public function successMessage($ret) {
+        return _('成功建立產品類型');
+    }
+
+}
+
+
