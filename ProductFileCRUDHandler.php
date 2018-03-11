8ad8eb82eb5fe23950fb107a363b216a2fbfd165
c9s <yoanlin93@gmail.com>
2018-03-11 18:25:20 +0800

    fix

--- ProductFileCRUDHandler.php
+++ ProductFileCRUDHandler.php
@@ -40,45 +40,12 @@ class ProductFileCRUDHandler extends CRUDHandler
         if ($this->canDelete) {
             $controls[] = ['action' => 'delete'];
         }
-
-        return [
-            "view" => "TextCoverView",
-            "display" => "block",
-            "title" => [ "field" => "title" ],
-            "subtitle" => [ "format" => "檔案: {file}" ],
-            "desc" => [ "field" => "description" ],
-            "footer" => [
-                "columns" => [
-                    [ "text" => [ 'format' => 'Created at: {created_at}' ] ],
-                    [ "text" => [ 'format' => 'Updated at: {updated_at}' ] ],
-                ]
-            ],
-            "controls" => $controls,
-        ];
-    }
-
-    /**
-     * itemDesc describes the relationship between data and the placeholder designed in the UI
-     * and defines how the cover view should be built
-     *
-     * @return array
-     */
-    public function itemDesc()
-    {
-        $controls = [];
-        if ($this->canUpdate) {
-            $controls[] = ['action' => 'edit'];
-        }
-        if ($this->canDelete) {
-            $controls[] = ['action' => 'delete'];
-        }
         return [
             "view" => "TextCoverView",
             "display" => "block",
             "title" => [ "field" => "title" ],
             "subtitle" => ["format" => "備註: {remark}"],
-
-            // "desc" => [ "field" => "description" ],
+            "desc" => [ "format" => "{file}" ],
             /*
             "footer" => [
                 "columns" => [
