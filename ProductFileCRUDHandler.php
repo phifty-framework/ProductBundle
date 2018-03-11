615b85a7db9c2ebf53cc4a550dc54701b4efe674
c9s <yoanlin93@gmail.com>
2018-03-11 18:10:39 +0800

    improve product file crud definition

--- ProductFileCRUDHandler.php
+++ ProductFileCRUDHandler.php
@@ -1,22 +1,58 @@
 <?php
 namespace ProductBundle;
+
 use AdminUI\CRUDHandler;
+use ProductBundle\Model\ProductFile;
 
 class ProductFileCRUDHandler extends CRUDHandler
 {
-    public $modelClass = 'ProductBundle\\Model\\ProductFile';
-    public $crudId     = 'product_file';
-    public $listColumns = array('id', 'title','file');
+    use \CRUD\CRUDReactHasManyEditor;
 
-    public function getModalActionView()
+    public $modelClass = ProductFile::class;
+
+    public $crudId     = 'product-file';
+
+    public $templateId     = 'product_file';
+
+    public $listColumns = ['id', 'title','file'];
+
+    protected $searchQueryFields = ['product_id'];
+
+    protected $applyRequestFields = ['product_id'];
+
+    /**
+     * itemDesc describes the relationship between data and the placeholder designed in the UI
+     * and defines how the cover view should be built
+     *
+     * @return array
+     */
+    public function itemDesc()
     {
-        $view = $this->createActionView($this->currentAction,null,array(
-            'submit_btn' => false,
-            'close_btn' => false,
-            'ajax' => true,
-            'skips' => array('product_id','mimetype'),
-        ));
-        return $view;
+        $controls = [];
+        if ($this->canCreate) {
+            $controls[] = ['action' => 'create'];
+        }
+        if ($this->canUpdate) {
+            $controls[] = ['action' => 'edit'];
+        }
+        if ($this->canDelete) {
+            $controls[] = ['action' => 'delete'];
+        }
+
+        return [
+            "view" => "TextCoverView",
+            "display" => "block",
+            "title" => [ "field" => "title" ],
+            "subtitle" => [ "format" => "檔案: {file}" ],
+            "desc" => [ "field" => "description" ],
+            "footer" => [
+                "columns" => [
+                    [ "text" => [ 'format' => 'Created at: {created_at}' ] ],
+                    [ "text" => [ 'format' => 'Updated at: {updated_at}' ] ],
+                ]
+            ],
+            "controls" => $controls,
+        ];
     }
 }
 
